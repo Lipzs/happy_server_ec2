81e0ce3928347f589e1f8f71509d74e8a23f14e3
Lipzs <filipedias@hotmail.com>
2022-11-04 20:10:46 -0300

    changes to deploy application

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "type": "postgres",
+  "host": process.env.DB_HOST,
+  "port": 5432,
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_ID,
+  "migrations": [
+    "./src/database/migrations/*.ts"
+  ],
+  "entities": [
+    "./src/models/*.ts"
+  ],
+  "cli": {
+    "migrationsDir": "./src/database/migrations"
+  }
+}
