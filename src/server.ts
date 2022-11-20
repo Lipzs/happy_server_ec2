697f6342d3c661fd02cf874c890bf00c47f21756
Lipzs <filipedias@hotmail.com>
2022-11-20 18:41:25 -0300

    middleware resolution

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,13 @@ import './database/connection';
 const app = express();
 
 app.use(cors());
+
+// a middleware with no mount path; gets executed for every request to the app
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next();
+});
+
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
