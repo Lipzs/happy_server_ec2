ce4a82122ae5a563f04512938d81ebdb7359315b
Lipzs <filipedias@hotmail.com>
2022-11-20 17:39:33 -0300

    change console log

--- src/server.ts
+++ src/server.ts
@@ -18,5 +18,5 @@ app.use(errorHandler);
 app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log('Server started!');
+  console.log(`Server started! on port ${process.env.PORT}`);
 });
