1dd91e10ed16fdf8185fdef0dae1fa076d687e23
Lipzs <filipedias@hotmail.com>
2022-11-22 21:23:26 -0300

    changes api

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 3333, () =>
+app.listen(3333, () =>
   console.log('🔥 Server started at http://localhost:3333')
 );
 
