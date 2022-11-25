3343bdbaeab5c3ea5ccec90d2b5e95189e167039
Lipzs <filipedias@hotmail.com>
2022-11-25 16:07:12 -0300

    changed migration

--- src/database/migrations/1603022834931-create_orphanages.ts
+++ src/database/migrations/1603022834931-create_orphanages.ts
@@ -21,15 +21,11 @@ export class createOrphanages1602603247723 implements MigrationInterface {
           },
           {
             name: 'latitude',
-            type: 'decimal',
-            scale: 10,
-            precision: 2,
+            type: 'varchar'
           },
           {
             name: 'longitude',
-            type: 'decimal',
-            scale: 10,
-            precision: 2,
+            type: 'varchar'
           },
           {
             name: 'about',
