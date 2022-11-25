9095b2d281b8aeb7f3dd3a7f85e3d8274151e490
Lipzs <filipedias@hotmail.com>
2022-11-25 20:53:37 -0300

    changed image view

--- src/views/images_view.ts
+++ src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://192.168.1.4:3333/uploads/${image.path}`,
+      url: `http://34.201.200.134:3333/uploads/${image.path}`,
     };
   },
 
